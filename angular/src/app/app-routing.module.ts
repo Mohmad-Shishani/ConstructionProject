babd9d8698a29b0d6883d682450f399b2efb9b33
mohammad sultan <mohammadsultan127@yahoo.com>
2022-01-17 14:38:06 +0200

    Angular_Basic_Stuff_Unintinional_Commit

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkerComponent } from './worker/worker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'workers', component: WorkerComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
